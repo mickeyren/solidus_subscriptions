573a0d9be27c2b69f5696d929f9aaaab9bbd54aa
Sherry Son <son.sherrysoomin92@gmail.com>
2016-06-08 09:59:29 -0400

    adjust default on subscription interval from nil to 0

--- db/migrate/20160607162054_update_interval_on_spree_subscriptions_default_to_be_zero.rb
+++ db/migrate/20160607162054_update_interval_on_spree_subscriptions_default_to_be_zero.rb
@@ -1,5 +1,5 @@
 class UpdateIntervalOnSpreeSubscriptionsDefaultToBeZero < ActiveRecord::Migration
   def change
-    add_column :spree_subscriptions, :interval, :integer, default: 0
+    change_column :spree_subscriptions, :interval, :integer, default: 0
   end
 end
