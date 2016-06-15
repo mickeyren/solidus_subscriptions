6cd121685c716251f405be1f581900e8ebd7f51a
Sherry Son <son.sherrysoomin92@gmail.com>
2016-06-15 17:00:54 -0400

    refactor logic

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -8,7 +8,7 @@ module Spree
     end
 
     def finalize_with_create_subscription!
-      return unless !user.nil?
+      return if user.nil?
       CreateSubscriptionJob.perform_later(self)
       finalize_without_create_subscription!
     end
