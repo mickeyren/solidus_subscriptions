d8a615a62742b9b103cd0acb2c8da9792ab8939b
Sherry Son <son.sherrysoomin92@gmail.com>
2016-06-20 09:04:19 -0400

    added service and specs for updating the addresses of a subscription

---
+++ app/services/update_subscription_address.rb
@@ -0,0 +1,28 @@
+class UpdateSubscriptionAddress
+
+  def update_address(subscription, subscription_params)
+    new_ship_address = subscription_params[:ship_address_attributes]
+    new_bill_address = subscription_params[:bill_address_attributes]
+
+    if (subscription.ship_address != new_ship_address) && (subscription.bill_address == new_bill_address)
+      update_ship_address(subscription, new_ship_address)
+    elsif (subscription.bill_address != new_bill_address) && (subscription.ship_address == new_ship_address)
+      update_bill_address(subscription, new_bill_address)
+    elsif (subscription.ship_address != new_ship_address) && (subscription.bill_address != new_bill_address)
+      update_ship_and_bill_address(subscription, new_ship_address, new_bill_address)
+    end
+  end
+
+  def update_ship_address(subscription, new_ship_address)
+    subscription.build_ship_address(subscription.ship_address.dup.attributes.merge(new_ship_address))
+  end
+
+  def update_bill_address(subscription, new_bill_address)
+    subscription.build_bill_address(subscription.bill_address.dup.attributes.merge(new_bill_address)))
+  end
+
+  def update_ship_and_bill_address(subscription, new_ship_address, new_bill_address)
+    update_ship_address(subscription, new_ship_address)
+    update_bill_address(subscription, new_bill_address)
+  end
+end
---
+++ spec/services/update_subscription_address_spec.rb
@@ -0,0 +1,83 @@
+require 'spec_helper'
+
+describe UpdateSubscriptionAddress do
+
+  context "when updating a subscription" do
+    let(:user) { create(:user) }
+    let(:old_address) { create(:subscription_address, address1: "Old Address", user: user) }
+    let(:subscription) { create(:subscription, ship_address: old_address, bill_address: old_address, user: user) }
+
+    before do
+      @new_address = build_address_params(old_address)
+      @new_address[:address1] = "New Address"
+    end
+
+    it "admin users are able to update only the ship address" do
+      subscription_params = {
+        "interval": subscription.interval,
+        "email": subscription.email,
+        "ship_address_attributes": @new_address,
+        "bill_address_attributes": old_address
+      }
+      UpdateSubscriptionAddress.new.update_address(subscription, subscription_params)
+
+      updated_ship_address_params = build_address_params(subscription.ship_address)
+
+      expect(subscription.ship_address.user_id).to eq(subscription.user_id)
+      expect(updated_ship_address_params).to eq(@new_address)
+
+      expect(subscription.bill_address).to eq(old_address)
+    end
+
+    it "admin users are able to update only the bill address" do
+      subscription_params = {
+        "interval": subscription.interval,
+        "email": subscription.email,
+        "ship_address_attributes": old_address,
+        "bill_address_attributes": @new_address
+      }
+      UpdateSubscriptionAddress.new.update_address(subscription, subscription_params)
+
+      updated_bill_address_params = build_address_params(subscription.bill_address)
+
+      expect(subscription.bill_address.user_id).to eq(subscription.user_id)
+      expect(updated_bill_address_params).to eq(@new_address)
+
+      expect(subscription.ship_address).to eq(old_address)
+    end
+
+    it "admin users are able to update both the ship and bill addresses" do
+      subscription_params = {
+        "interval": subscription.interval,
+        "email": subscription.email,
+        "ship_address_attributes": @new_address,
+        "bill_address_attributes": @new_address
+      }
+      UpdateSubscriptionAddress.new.update_address(subscription, subscription_params)
+
+      updated_ship_address_params = build_address_params(subscription.ship_address)
+      updated_bill_address_params = build_address_params(subscription.bill_address)
+
+      expect(subscription.ship_address.user_id).to eq(subscription.user_id)
+      expect(updated_ship_address_params).to eq(@new_address)
+
+      expect(subscription.bill_address.user_id).to eq(subscription.user_id)
+      expect(updated_bill_address_params).to eq(@new_address)
+
+    end
+  end
+
+  def build_address_params(address)
+    address = {
+      firstname: address.firstname,
+      lastname: address.lastname,
+      address1: address.address1,
+      address2: address.address2,
+      zipcode: address.zipcode,
+      country_id: address.country_id,
+      state_id: address.state_id,
+      phone: address.phone
+    }
+  end
+
+end
