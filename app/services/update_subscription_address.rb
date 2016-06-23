c8c3a6a7be5586707b0a9f76cb2cc8eb690a770d
Sherry Son <son.sherrysoomin92@gmail.com>
2016-06-23 15:16:57 -0400

    refactor update address methods

--- app/services/update_subscription_address.rb
+++ app/services/update_subscription_address.rb
@@ -4,14 +4,10 @@ class UpdateSubscriptionAddress
     new_ship_address = subscription_params[:ship_address_attributes]
     new_bill_address = subscription_params[:bill_address_attributes]
 
-    if (subscription.ship_address != new_ship_address) && (subscription.bill_address == new_bill_address)
-      update_ship_address(subscription, new_ship_address)
-    elsif (subscription.bill_address != new_bill_address) && (subscription.ship_address == new_ship_address)
-      update_bill_address(subscription, new_bill_address)
-    elsif (subscription.ship_address != new_ship_address) && (subscription.bill_address != new_bill_address)
-      update_ship_and_bill_address(subscription, new_ship_address, new_bill_address)
-    end
+    update_ship_address(subscription, new_ship_address) if subscription.ship_address != new_ship_address
+    update_bill_address(subscription, new_bill_address) if subscription.bill_address != new_bill_address
   end
+
 
   def update_ship_address(subscription, new_ship_address)
     subscription.ship_address.update_columns(new_ship_address)
@@ -21,8 +17,4 @@ class UpdateSubscriptionAddress
     subscription.bill_address.update_columns(new_bill_address)
   end
 
-  def update_ship_and_bill_address(subscription, new_ship_address, new_bill_address)
-    update_ship_address(subscription, new_ship_address)
-    update_bill_address(subscription, new_bill_address)
-  end
 end
