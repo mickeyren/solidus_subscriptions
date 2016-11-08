4e1688ac64fdb43bf2de73a893f885e4fe0175c1
David Ang <davidang09@gmail.com>
2016-11-09 00:20:10 +0800

    migration to add the renewal dates

---
+++ db/migrate/20160711000407_add_renewal_dates_to_subscriptions.rb
@@ -0,0 +1,12 @@
+class AddRenewalDatesToSubscriptions < ActiveRecord::Migration
+  def change
+    add_column :spree_subscriptions, :next_renewal_at, :datetime
+    add_column :spree_subscriptions, :last_renewal_at, :datetime
+
+    Spree::Subscription.active.each do |subscription|
+      subscription.update_column(:last_renewal_at,
+        subscription.last_order.completed_at)
+      subscription.touch
+    end
+  end
+end
